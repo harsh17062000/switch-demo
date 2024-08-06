3610e41239f5c276dc33a2aff2f154c37bf2f7ce
harsh17062000 <harshgupt100@gmail.com>
2024-08-06 15:44:09 +0530

    switch case

---
+++ switch-case-fall.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "enter the month " << "\n";
+    cout << " press 1 for jan , 2 for feb ..... 12 for dec" << "\n";
+    int month=0;
+    cin >> month;
+    
+    switch(month){
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+    cout << " 31 days " << "/n ";
+    break;
+    case 2:
+    cout << "28 or 29 days";
+    break;
+
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+    cout << "30 days" << "\n ";
+    break;
+    default:
+    cout << " invalid " ;
+
+}
+}
